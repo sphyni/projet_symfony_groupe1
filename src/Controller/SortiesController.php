6de4f3381123a64d3a6424c86d23b5a754939f0e
Florian Baron <florian.baron2020@campus-eni.fr>
2021-03-16 16:53:51 +0100

    Création de 4 controller et de leurs templates

---
+++ src/Controller/SortiesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortiesController extends AbstractController
+{
+    /**
+     * @Route("/sorties", name="sorties")
+     */
+    public function index(): Response
+    {
+        return $this->render('sorties/index.html.twig', [
+            'controller_name' => 'SortiesController',
+        ]);
+    }
+}
