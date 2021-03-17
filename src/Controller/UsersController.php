e759509b8a5c82b55059ad57bb6b7b4c99d1cb4c
sphyni <anatole.clerc@yahoo.fr>
2021-03-17 11:31:09 +0100

    commit pour récupéré le mainController

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,19 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Form\MonProfileType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UsersController extends AbstractController
 {
+
+
     /**
-     * @Route("/users", name="users")
+     * @Route("/profile/{id}", name="profile", requirements={"id":"\d+"})
      */
-    public function index(): Response
+    public function details(int $id, EntityManagerInterface $em):response
     {
-        return $this->render('users/index.html.twig', [
-            'controller_name' => 'UsersController',
-        ]);
+
+        $repository=$em->getRepository(Participant::class);
+        $user =$repository->find($id);
+
+        return $this->render('');
+
     }
+
+    /**
+     * @Route("/monprofile", name="monProfile")
+     */
+    public function create(Request $request, EntityManagerInterface $em):response {
+        $user = new Participant;
+        $monProfilForm = $this->createForm(MonProfileType::class, $user);
+
+        $monProfilForm->handleRequest($request);
+
+        if($monProfilForm->isSubmitted() && $monProfilForm->isValid()) {
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'profile modifié');
+            return $this->redirectToRoute('#');
+        }
+
+    }
+
 }
