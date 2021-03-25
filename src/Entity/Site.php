4eb17d4ae97f5afaf4678ff37d6eaf3ce96fc19b
sphyni <anatole.clerc@yahoo.fr>
2021-03-25 16:44:22 +0100

    correction le bousin remarche a peu pres sans béquille

--- src/Entity/Site.php
+++ src/Entity/Site.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\SiteRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=SiteRepository::class)
